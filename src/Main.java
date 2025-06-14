c88bde2e8c937bf8011d9876e0bd5d70538a88d5
olababiuch <ola.babiuch7@gmail.com>
2025-06-14 02:37:29 -0400

    Completed part 1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,29 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int intOperandA = 1;
+        int intOperandB = 3;
+        int intSum = 24;
+        int intProduct = 18;
+        int intDifference = 15;
+        int intQuotient = 4;
+        int intModulo = 2;
+        double doubleOperandA = 1.5;
+        double doubleOperandB = 2.5;
+        double doubleSum = 24.5;
+        double doubleProduct = 18.5;
+        double doubleDifference = 15.0;
+        double doubleQuotient = 4.5;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+
+
+
+
+        int kidsInFamily = 6;
+        System.out.println("The number of kids in my family is " + kidsInFamily);
+        boolean isRaining = false;
     }
 }
